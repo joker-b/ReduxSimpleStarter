a854f03004347672ff8bbd776c47f7c50a7c5d54
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2022-01-14 15:15:05 -0800

    selections okay : 261

--- src/index.js
+++ src/index.js
@@ -14,18 +14,23 @@ const API_KEY = 'AIzaSyAkRrZ5U1WL242T5hKbccIUlKlaEp-rUMA';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: []};
+        this.state = { videos: [],
+                       selectedVideo: null };
         YTSearch({key: API_KEY, term: 'Contax'},
         (videos) => {
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]});
         });
     }
     render() {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>
         );
     }
