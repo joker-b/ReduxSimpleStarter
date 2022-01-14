679c481e077c44b0f13282570c0e6e3cdfb283e2
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2022-01-14 13:55:37 -0800

    Update search_bar.js
    unit 252

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: 'duh'
+    };
+  }
   render() {
     return (
       <div className="search-bar">
         <input
+          value = {this.state.term}
+          onChange={event => this.setState({term: event.target.value})}
           className="search-bar__input"
           type="text"
           placeholder="Search for a movie, TV show, or person..."
-        />
-        <button className="search-bar__button">
-          <i className="fas fa-search" />
-        </button>
+        />        
       </div>
     );
   }
