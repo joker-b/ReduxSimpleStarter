30636ccfd9f88cf9bb648fd448bc45c1ef6852d7
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2022-01-14 14:40:44 -0800

    temporary kittens

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const VideoListItem = (props) => {
+const VideoListItem = (video) => {
+    console.log(video);
+    const imgUrl = "http://placekitten.com/80/64"; // video.snippet.thumbnails.default.url;
+    const title = "Title"; // video.snippet.title;
     return (
         <li className="list-group-item">
-            Video {props.video.snippet.title}
+            <div className="video-list-media">
+                <div className="media-left">
+                    <img className="media-object" src={imgUrl} />
+                </div>
+                <div className="media-right">
+                    <div className="media-heading">{title}</div>
+                </div>
+            </div>
         </li>
     );
 };
